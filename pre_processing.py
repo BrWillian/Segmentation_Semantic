d9ae1760d85ed7e3e06aeb6d09b084db47462c76
Willian <wiliam-m-@hotmail.com>
2020-03-06 12:18:50 -0300

    Refined bugs

--- pre_processing.py
+++ pre_processing.py
@@ -4,11 +4,6 @@
 import sys
 import os
 
-train_dir = sys.argv[2] + 'train/'
-train_masks_dir = sys.argv[2] + 'train_masks/'
-valid_dir = sys.argv[2] + 'valid/'
-valid_mask_dir = sys.argv[2] + 'valid_masks/'
-
 
 def simple_generator():
     i = 0
@@ -16,6 +11,14 @@ def simple_generator():
         i += 1
         yield i
 
+
+def dirs():
+    train_dir = sys.argv[2] + 'train/'
+    train_masks_dir = sys.argv[2] + 'train_masks/'
+    valid_dir = sys.argv[2] + 'valid/'
+    valid_mask_dir = sys.argv[2] + 'valid_masks/'
+
+    return train_dir, train_masks_dir, valid_dir, valid_mask_dir
 
 def verify_dir():
     if not os.path.isdir(sys.argv[2]):
@@ -34,10 +37,11 @@ def list_dir(dir):
 
 
 def pre_processing():
+    train_dir, train_masks_dir, valid_dir, valid_masks_dir = dirs()
     data_train = list_dir(train_dir)
     data_mask = list_dir(train_masks_dir)
     valid = list_dir(valid_dir)
-    valid_mask = list_dir(valid_mask_dir)
+    valid_mask = list_dir(valid_masks_dir)
     gen_train = simple_generator()
     gen_valid = simple_generator()
 
@@ -55,8 +59,13 @@ def pre_processing():
     for x, y in zip(valid, valid_mask):
         id = next(gen_valid)
         os.rename(valid_dir + x + '.jpg', valid_dir + 'img_{}.jpg'.format(id))
-        os.rename(valid_mask_dir + y + '.png', valid_mask_dir + 'img_{}_mask.png'.format(id))
+        os.rename(valid_masks_dir + y + '.png', valid_masks_dir + 'img_{}_mask.png'.format(id))
 
 
 if __name__ == '__main__':
-    pre_processing()
+
+    if len(sys.argv) != 3:
+        print('Usage python3 pre-processing.py --dir [Dataset path (train, train_masks, valid, valid_masks]')
+        exit()
+    elif not verify_dir():
+        pre_processing()
